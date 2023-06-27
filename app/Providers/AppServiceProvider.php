fa837f11d8e256d77ea3607095741bf12dee473b
Boyan Balkanski <boyan.balkanski@asap.bg>
2023-06-27 12:06:30 +0300

    Remove unneeded sql setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +28,5 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFour();
         Model::unguard(true);
-        Schema::defaultStringLength(191);
     }
 }
