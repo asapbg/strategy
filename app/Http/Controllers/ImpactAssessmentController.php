a514791e99921b356bb4a1e92566e1b4cebae1a1
Boyan Balkanski <boyan.balkanski@asap.bg>
2023-08-06 11:54:46 +0300

    Completed Form 1

--- app/Http/Controllers/ImpactAssessmentController.php
+++ app/Http/Controllers/ImpactAssessmentController.php
@@ -16,16 +16,20 @@ class ImpactAssessmentController extends Controller
 
     public function store($formName)
     {
+        $state = session("forms.$formName", []);
         $data = app('request')->except('_token');
         if (array_key_exists('add_entry', $data)) {
             $data[substr($data['add_entry'], 0, -2)][] = '';
+            unset($data['add_entry']);
         }
         if (array_key_exists('add_array_entry', $data)) {
             $key = $data['add_array_entry'];
             $value = data_get($data, $key, []);
             array_push($value, []);
             data_set($data, $key, $value);
+            unset($data['add_array_entry']);
         }
+        $data = array_merge($state, $data);
         session(["forms.$formName" => $data]);
         return redirect()->back();
     }
--- resources/views/impact_assessment/form1.blade.php
+++ resources/views/impact_assessment/form1.blade.php
@@ -73,6 +73,192 @@
                 @include('form_partials.form1.comparison')
             </td>
         </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6. Избор на препоръчителен вариант:</h4>
+            </td>
+        </tr>
+        @php($loop = array_key_exists('problem_to_solve', $state) ? count($state['problem_to_solve']) : 1)
+        @for($n=0; $n<$loop; $n++)
+        <tr>
+            <td>
+                <h5>{{ __('forms.on_problem') . ' ' . $n+1 . ': ' . __('forms.variant') }}</h5>
+            </td>
+            <td>
+                @include('form_partials.text', ['name' => 'chosen_variants[]', 'label' => ''])
+            </td>
+        </tr>
+        @endfor
+        <tr>
+            <td colspan="2">
+                <p class="text-center">
+                    <i>Посочете препоръчителните варианти за решаване на поставения проблем/проблеми.</i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6.1. Промяна в административната тежест за физическите и юридическите лица от прилагането на препоръчителния вариант (включително по отделните проблеми):</h4>   
+                @include('form_partials.radio', ['name' => 'change_admin_weight', 'value' => 0, 'label' => 'forms.will_increase'])
+                <br>
+                @include('form_partials.radio', ['name' => 'change_admin_weight', 'value' => 1, 'label' => 'forms.will_decrease'])
+                <br>
+                @include('form_partials.radio', ['name' => 'change_admin_weight', 'value' => 2, 'label' => 'forms.no_effect'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'change_admin_weight_text', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        1.1. Изборът следва да е съотносим с посочените специфични въздействия на препоръчителния вариант за решаване на всеки проблем.
+                        <br>
+                        1.2. Ако се предвижда въвеждането на такса, представете образуването на нейния размер съгласно Методиката по чл. 7а от Закона за ограничаване на административното регулиране и административния контрол върху стопанската дейност.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6.2. Създават ли се нови/засягат ли се съществуващи регулаторни режими и услуги от прилагането на препоръчителния вариант (включително по отделните проблеми)?</h4>   
+                @include('form_partials.radio', ['name' => 'affects_regulatory_acts', 'value' => 1, 'label' => 'forms.yes'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'affects_regulatory_acts_text', 'label' => ''])
+                <br>
+                @include('form_partials.radio', ['name' => 'affects_regulatory_acts', 'value' => 0, 'label' => 'forms.no'])
+                <p class="text-center">
+                    <i>
+                        1.1. Изборът следва да е съотносим с посочените специфични въздействия на избрания вариант.
+                        <br>
+                        1.2. В случай че се предвижда създаване нов регулаторен режим, посочете неговия вид (за стопанска дейност: лицензионен, регистрационен; за отделна стелка или действие: разрешителен, уведомителен; удостоверителен и по какъв начин това съответства с постигането на целите).
+                        <br>
+                        1.3. Мотивирайте създаването на новия регулаторен режим съгласно изискванията на чл. 3, ал. 4  от Закона за ограничаване на административното регулиране и административния контрол върху стопанската дейност.
+                        <br>
+                        1.4. Посочете предложените нови регулаторни режими отговарят ли на изискванията на чл. 10 – 12 от Закона за дейностите по предоставяне на услуги.
+                        <br>
+                        1.5. Посочете изпълнено ли е изискването на § 2 от Допълнителните разпоредби на Закона за дейностите по предоставяне на услуги.
+                        <br>
+                        1.6. В случай че се изменят регулаторни режими или административни услуги, посочете промяната.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6.3. Създават ли се нови регистри от прилагането на препоръчителния вариант (включително по отделните проблеми)?</h4>   
+                @include('form_partials.radio', ['name' => 'affects_registry', 'value' => 1, 'label' => 'forms.yes'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'affects_registry_text', 'label' => ''])
+                <br>
+                @include('form_partials.radio', ['name' => 'affects_registry', 'value' => 0, 'label' => 'forms.no'])
+                <p class="text-center">
+                    <i>
+                        Когато отговорът е „Да“, посочете регистрите, които се създават и по какъв начин те ще бъдат интегрирани в общата регистрова инфраструктура.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6.4. По какъв начин препоръчителният вариант въздейства върху микро-, малките и средните предприятия (МСП) (включително по отделните проблеми)?</h4>   
+                @include('form_partials.radio', ['name' => 'affects_companies', 'value' => 1, 'label' => 'forms.act_affects_smc'])
+                <br>
+                @include('form_partials.radio', ['name' => 'affects_companies', 'value' => 0, 'label' => 'forms.act_not_affects_smc'])
+                <p class="text-center">
+                    <i>
+                        Изборът следва да е съотносим с посочените специфични въздействия на препоръчителния вариант.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>6.5. Потенциални рискове от прилагането на препоръчителния вариант (включително по отделните проблеми):</h4>
+                @include('form_partials.textarea', ['name' => 'potential_risks', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        Посочете възможните рискове от прилагането на препоръчителния вариант, различни от отрицателните въздействия, напр. възникване на съдебни спорове и др.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>7. Консултации:</h4>
+                @include('form_partials.radio', ['name' => 'conducted_consultations', 'value' => 1, 'label' => 'forms.conducted_consultations'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'conducted_consultations_text', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        Посочете основните заинтересовани страни, с които са проведени консултации. Посочете резултатите от консултациите, включително на ниво ЕС: спорни въпроси, многократно поставяни въпроси и др.
+                    </i>
+                </p>
+                @include('form_partials.radio', ['name' => 'not_conducted_consultations', 'value' => 0, 'label' => 'forms.not_conducted_consultations'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'not_conducted_consultations_text', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        Обобщете най-важните въпроси за обществени консултации. Посочете индикативен график за тяхното провеждане и видовете консултационни процедури.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>8. Приемането на нормативния акт произтича ли от правото на Европейския съюз?</h4>   
+                @include('form_partials.radio', ['name' => 'is_from_eu', 'value' => 1, 'label' => 'forms.yes'])
+                <br>
+                @include('form_partials.radio', ['name' => 'is_from_eu', 'value' => 0, 'label' => 'forms.no'])
+                <br>
+                @include('form_partials.textarea', ['name' => 'is_from_eu_text', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        1.1. Посочете изискванията на правото на Европейския съюз, включително информацията по т. 6.2 и 6.3, дали е извършена оценка на въздействието на ниво Европейски съюз, и я приложете (или посочете връзка към източник).
+                        <br>
+                        1.2. Изборът трябва да съответства на посоченото в раздел 1, съгласно неговата т. 1.5.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>9.  Изисква ли се извършване на цялостна предварителна оценка на въздействието поради очаквани значителни последици?</h4>   
+                @include('form_partials.radio', ['name' => 'initial_assessment_required', 'value' => 1, 'label' => 'forms.yes'])
+                <br>
+                @include('form_partials.radio', ['name' => 'initial_assessment_required', 'value' => 0, 'label' => 'forms.no'])
+                <p class="text-center">
+                    <i>
+                        (преценка съгласно чл. 20, ал. 3, т. 2 от Закона за нормативните актове)
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>10. Приложения:</h4>
+                @include('form_partials.textarea', ['name' => 'applications', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        Приложете необходимата допълнителна информация и документи
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>11. Информационни източници:</h4>
+                @include('form_partials.textarea', ['name' => 'info_sources', 'label' => ''])
+                <p class="text-center">
+                    <i>
+                        Посочете изчерпателен списък на информационните източници, които са послужили за оценка на въздействията на отделните варианти и при избора на вариант за действие: регистри, бази данни, аналитични материали и др.
+                    </i>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h4>12. Име, длъжност, дата и подпис на директора на дирекцията, отговорна за извършването на частичната предварителна оценка на въздействието:</h4>
+                @include('form_partials.text', ['name' => 'name_and_job', 'label' => 'forms.name_and_job'])
+                @include('form_partials.text', ['name' => 'date', 'label' => 'forms.date'])
+            </td>
+        </tr>
     </table>
     
     <button type="submit">{{ __('custom.save') }}</button>
