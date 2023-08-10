0f47c2cff190f0ff6e04df7175060e735a1e3ad2
Boyan Balkanski <boyan.balkanski@asap.bg>
2023-08-10 16:52:16 +0300

    More visual enhancements Form 1

--- resources/views/form_partials/label.blade.php
+++ resources/views/form_partials/label.blade.php
@@ -1,1 +1,1 @@
-<label for="{{ $name }}" class="mt-4 mb-2">{{ __(isset($label) ? $label : "forms.$name") }}</label>
+<label for="{{ $name }}" class="mb-2 mt-2">{{ __(isset($label) ? $label : "forms.$name") }}</label>
