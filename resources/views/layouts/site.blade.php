513cb630c2b0857a38948e80a9c4ee6fd51d9e0e
Boyan Balkanski <boyan.balkanski@asap.bg>
2023-08-17 16:06:00 +0300

    Design rollback

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -8,7 +8,7 @@
 
 
     <!-- Bootstrap  CSS -->
- <link href="{{ asset('/vendor/bootstrap/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+ <link href="{{ asset('/vendor/bootstrap/bootstrap.css') }}" rel="stylesheet" crossorigin="anonymous">
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 
