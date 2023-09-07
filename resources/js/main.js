804960fe9f77664971815fd8b05eac338576400d
ASAP <noreply@asap.bg>
2023-09-07 22:22:34 +0300

    f

--- resources/js/main.js
+++ resources/js/main.js
@@ -259,6 +259,30 @@ $(document).on("select2:open", () => {
 })
 
 $(document).ready(function (e) {
+    $.datepicker.regional = {
+        bg: {
+            days: ["Неделя", "Понеделник", "Вторник", "Сряда", "Четвъртък", "Петък", "Събота", "Неделя"],
+            daysShort: ["Нед", "Пон", "Вт", "Ср", "Чет", "Пет", "Съб", "Нед"],
+            daysMin: ["Н", "П", "В", "С", "Ч", "П", "С", "н"],
+            months: ["Януари","Февруари","Март","Април","Май","Юни","Юли","Август","Септември","Октомври","Ноември","Декември"],
+            monthsShort: ["Ян","Фев","Мар","Апр","Май","Юн","Юл","Авг","Сеп","Окт","Ное","Дек"],
+            today: "Днес",
+            clear: "Изчисти"
+        },
+
+        en: {
+            days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+            months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            today: "Today",
+            clear: "Clear"
+        }
+
+    };
+
+    // $.datepicker.setDefaults( $.datepicker.regional[typeof GlobalLang != 'undefined' ? GlobalLang : ''] );
 
     $('.table-bordered tbody tr:even').not($('.dataTable tbody tr, .table-with-toggle-content tr')).addClass('odd');
 
@@ -377,6 +401,7 @@ $(document).ready(function (e) {
 
     if($('.datepicker').length) {
         $('.datepicker').datepicker({
+            language: typeof GlobalLang != 'undefined' ? GlobalLang : 'en',
             format: 'dd-mm-yyyy',
             todayHighlight: true,
             orientation: "bottom left",
@@ -386,6 +411,7 @@ $(document).ready(function (e) {
 
     if($('.datepicker-month').length) {
         $('.datepicker-month').datepicker({
+            language: typeof GlobalLang != 'undefined' ? GlobalLang : 'en',
             format: 'mm-yyyy',
             viewMode: "months",
             minViewMode: "months",
