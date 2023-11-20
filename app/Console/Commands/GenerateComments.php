16cf3bf214bfb6b316cd90e33934cf0078308414
ASAP <noreply@asap.bg>
2023-11-20 18:49:37 +0200

    f

---
+++ app/Console/Commands/GenerateComments.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateComments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:comments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate comments report after consultation ends';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //TODO
+    }
+}
