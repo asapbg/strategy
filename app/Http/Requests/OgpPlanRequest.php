c68667b1707e6c27be021a46a4ad9910563006ed
Nikolay Mitrev <nikolay.mitrev@asap.bg>
2023-12-19 15:40:18 +0200

    fixes OGP

---
+++ app/Http/Requests/OgpPlanRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\ActType;
+use App\Models\OgpArea;
+use App\Models\OgpPlan;
+use Illuminate\Foundation\Http\FormRequest;
+
+class OgpPlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'from_date' => 'required',
+            'to_date' => 'required',
+            'active' => '',
+            'ogp_area' => 'required|gt:0',
+        ];
+
+        foreach (config('available_languages') as $lang) {
+            foreach (OgpPlan::translationFieldsProperties() as $field => $properties) {
+                $rules[$field.'_'.$lang['code']] = $properties['rules'];
+            }
+        }
+
+        return $rules;
+    }
+}
