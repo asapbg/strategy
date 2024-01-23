748496913ea0af7a31545af02f6c3422f9c3fa45
ASAP <noreply@asap.bg>
2024-01-23 15:13:05 +0200

    adv board members clear admin modals layouts

--- resources/views/admin/advisory-boards/modals/edit-member-form.blade.php
+++ resources/views/admin/advisory-boards/modals/edit-member-form.blade.php
@@ -21,112 +21,6 @@
                     <div class="row">
                         @include('admin.partial.edit_field_translate', ['translatableFields' => \App\Models\AdvisoryBoardMember::translationFieldsProperties(), 'field' => 'member_name', 'required' => true])
                     </div>
-{{--                    <div class="row">--}}
-{{--                        <div class="col-md-6 col-12">--}}
-{{--                            <div class="form-group">--}}
-{{--                                <label class="col-sm-12 control-label"--}}
-{{--                                       for="member_name_bg">{{ __('custom.first_name') }} (BG) <span--}}
-{{--                                        class="required">*</span></label>--}}
-{{--                                <div class="row">--}}
-{{--                                    <div class="col-12">--}}
-{{--                                        <input type="text" id="member_name_bg" name="member_name_bg"--}}
-{{--                                               class="form-control form-control-sm"--}}
-{{--                                               value="" autocomplete="off"/>--}}
-
-{{--                                        <div class="text-danger mt-1 error_member_name_bg"></div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="col-md-6 col-12">--}}
-{{--                            <div class="form-group">--}}
-{{--                                <label class="col-sm-12 control-label"--}}
-{{--                                       for="member_name_en">{{ __('custom.first_name') }} (EN) <span--}}
-{{--                                        class="required">*</span></label>--}}
-{{--                                <div class="row">--}}
-{{--                                    <div class="col-12">--}}
-{{--                                        <input type="text" id="member_name_en" name="member_name_en"--}}
-{{--                                               class="form-control form-control-sm"--}}
-{{--                                               value="" autocomplete="off"/>--}}
-
-{{--                                        <div class="text-danger mt-1 error_member_name_en"></div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="row mb-3">--}}
-{{--                        <label class="col-sm-12 control-label"--}}
-{{--                               for="member_name_en">{{ __('custom.type') }}</label>--}}
-
-{{--                        <div class="col-12">--}}
-{{--                            <div class="row">--}}
-{{--                                <div class="col-auto">--}}
-{{--                                    <div class="form-check pl-4">--}}
-{{--                                        <input class="form-check-input" type="radio"--}}
-{{--                                               value="{{ \App\Enums\AdvisoryTypeEnum::MEMBER->value }}"--}}
-{{--                                               name="advisory_type_id"--}}
-{{--                                               id="edit_advisory_type_member"/>--}}
-{{--                                        <label class="form-check-label" id="" for="edit_advisory_type_member">--}}
-{{--                                            {{ trans_choice('custom.member', 1) }}--}}
-{{--                                        </label>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="col-auto">--}}
-{{--                                    <div class="form-check pl-4">--}}
-{{--                                        <input class="form-check-input" type="radio"--}}
-{{--                                               value="{{ \App\Enums\AdvisoryTypeEnum::CHAIRMAN->value }}"--}}
-{{--                                               name="advisory_type_id"--}}
-{{--                                               id="edit_advisory_type_chairman"/>--}}
-{{--                                        <label class="form-check-label" id="" for="edit_advisory_type_chairman">--}}
-{{--                                            {{ __('custom.chairman') }}--}}
-{{--                                        </label>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="col-auto">--}}
-{{--                                    <div class="form-check pl-4">--}}
-{{--                                        <input class="form-check-input" type="radio"--}}
-{{--                                               value="{{ \App\Enums\AdvisoryTypeEnum::VICE_CHAIRMAN->value }}"--}}
-{{--                                               name="advisory_type_id"--}}
-{{--                                               id="edit_advisory_type_vice_chairman"/>--}}
-{{--                                        <label class="form-check-label" id="" for="edit_advisory_type_vice_chairman">--}}
-{{--                                            {{ __('validation.attributes.vice_chairman') }}--}}
-{{--                                        </label>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="col-auto">--}}
-{{--                                    <div class="form-check pl-4">--}}
-{{--                                        <input class="form-check-input" type="radio"--}}
-{{--                                               value="{{ \App\Enums\AdvisoryTypeEnum::SECRETARY->value }}"--}}
-{{--                                               name="advisory_type_id"--}}
-{{--                                               id="edit_advisory_type_secretary"/>--}}
-{{--                                        <label class="form-check-label" id="" for="edit_advisory_type_secretary">--}}
-{{--                                            {{ trans_choice('custom.secretary', 1) }}--}}
-{{--                                        </label>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="row" id="member-checkbox">--}}
-{{--                        <div class="col-12">--}}
-{{--                            <div class="form-check pl-4">--}}
-{{--                                <input class="form-check-input" type="checkbox"--}}
-{{--                                       value="{{ \App\Enums\AdvisoryTypeEnum::MEMBER->value }}"--}}
-{{--                                       name="is_member"--}}
-{{--                                />--}}
-{{--                                <label class="form-check-label" id="" for="is_member">--}}
-{{--                                    {{ trans_choice('custom.member', 1) }}--}}
-{{--                                </label>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
 
                     <div class="row">
                         <div class="col-12">
@@ -157,54 +51,10 @@
                     <div class="row">
                         @include('admin.partial.edit_field_translate', ['translatableFields' => \App\Models\AdvisoryBoardMember::translationFieldsProperties(), 'field' => 'member_job'])
                     </div>
-{{--                    <div class="row">--}}
-{{--                        <div class="col-12">--}}
-{{--                            <div class="row">--}}
-{{--                                <div class="col-md-6 col-12">--}}
-{{--                                    <div class="form-group">--}}
-{{--                                        <label class="control-label" for="member_job_bg">--}}
-{{--                                            {{ __('forms.job') }} (BG)--}}
-{{--                                        </label>--}}
-
-{{--                                        <input type="text" id="member_job_bg"--}}
-{{--                                               name="member_job_bg"--}}
-{{--                                               class="form-control form-control-sm"/>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="col-md-6 col-12">--}}
-{{--                                    <div class="form-group">--}}
-{{--                                        <label class="control-label" for="member_job_en">--}}
-{{--                                            {{ __('forms.job') }} (EN)--}}
-{{--                                        </label>--}}
-
-{{--                                        <input type="text" id="member_job_en"--}}
-{{--                                               name="member_job_en"--}}
-{{--                                               class="form-control form-control-sm"/>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
 
                     <div class="row">
                         @include('admin.partial.edit_field_translate', ['translatableFields' => \App\Models\AdvisoryBoardMember::translationFieldsProperties(), 'field' => 'member_notes'])
                     </div>
-
-{{--                    <div class="row mb-2">--}}
-{{--                        @foreach(config('available_languages') as $lang)--}}
-{{--                            <div class="col-6">--}}
-{{--                                <label for="member_notes_{{ $lang['code'] }}">--}}
-{{--                                    {{ __('validation.attributes.member_notes') }}--}}
-{{--                                    ({{ Str::upper($lang['code']) }})--}}
-{{--                                </label>--}}
-
-{{--                                <textarea class="form-control form-control-sm summernote"--}}
-{{--                                          name="member_notes_{{ $lang['code'] }}"--}}
-{{--                                          id="member_notes_{{ $lang['code'] }}"></textarea>--}}
-{{--                            </div>--}}
-{{--                        @endforeach--}}
-{{--                    </div>--}}
 
                     <div class="row">
                         <div class="col-12">
