b29e12cdecc3b03b334a57badea86ea0519641ba
ASAP <noreply@asap.bg>
2024-03-12 17:41:35 +0200

    fix

--- resources/views/site/ogp/forum.blade.php
+++ resources/views/site/ogp/forum.blade.php
@@ -8,7 +8,7 @@
         <div class="col-lg-10 right-side-content py-5" id="listContainer">
             <div class="row mb-4 ks-row">
                 <div class="col-md-12">
-                    <h2 class="mb-2">{{ __('custom.information') }} <a class="fs-16" href="{{ route('advisory-boards.view', $item) }}" target="_blank">({{ $item->name }})</a></h2>
+                    <h2 class="mb-2">{{ __('custom.information') }} @if($item->id) <a class="fs-16" href="{{ route('advisory-boards.view', $item) }}" target="_blank">({{ $item->name }})</a> @endif</h2>
                 </div>
             </div>
 
@@ -300,7 +300,7 @@
                     </div>
                 </div>
             @endif
-            
+
 
             @if($item->moderatorInformation || $item->moderators->count())
                 <div class="row mb-4 ks-row">
