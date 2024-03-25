61d62a02797a8193ee41f162462bab5bac1ea7b8
ASAP <noreply@asap.bg>
2024-03-25 14:26:54 +0200

    STRATEGY-247

--- resources/views/exports/ogp_national_plan.blade.php
+++ resources/views/exports/ogp_national_plan.blade.php
@@ -20,7 +20,7 @@
     <body style="text-align: left;padding-left: 50px; padding-right: 50px;">
         <div style="font-size: 22px;">
 @endif
-            <h3 style="text-align: center">{{ $data['title'] }}</h3>
+            <h3 style="text-align: center;margin-bottom: 100px;">{{ $data['title'] }}</h3>
             {{ htmlToText($data['content']) }}
             <br>
             <h4>ТЕМАТИЧНИ ОБЛАСТИ</h4>
@@ -35,8 +35,8 @@
                         @foreach($areas->arrangements as $keyA => $arrangement)
                             <h5>{{ $key + 1 }}.1.{{ $keyA + 1 }} {{ $arrangement->name }}</h5>
                             <p><strong>{{ __('custom.period') }}:</strong> {{ $arrangement->from_date }} - {{ $arrangement->to_date }}</p>
-                            <p><strong>{{ __('ogp.responsible_administration') }}:</strong> {{ $arrangement->responsible_administration }}</p>
-                            <p><strong>{{ __('ogp.npo_partner') }}:</strong> {{ $arrangement->npo_partner }}</p>
+                            <p><strong>{{ __('ogp.responsible_administration') }}:</strong> {!! $arrangement->responsible_administration !!}</p>
+                            <p><strong>{{ __('ogp.npo_partner') }}:</strong> {!! $arrangement->npo_partner !!}</p>
                             {!! $arrangement->content !!}
 {{--                            {{ htmlToText($arrangement->content) }}--}}
                         @endforeach
