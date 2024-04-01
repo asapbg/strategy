2c26674c33650d04e99e4675a0027a07b6902c77
ASAP <noreply@asap.bg>
2024-04-01 17:22:44 +0300

    STRATEGY-202 STRATEGY-198

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6 register-form p-4">
             <h2 class="fs-3 mb-3 text-center">Регистрация в системата</h2>
-            <form method="POST" action="{{ route('register') }}">
+            <form method="POST" action="{{ route('register') }}" id="reg_form">
                 @csrf
                 <div class="row mb-3">
                     <label for="is_org" class="col-md-12 col-form-label text-left mb-2 fs-5">{{ __('validation.attributes.is_org') }} <span class="text-danger">*</span></label>
@@ -94,12 +94,14 @@
                 </div>
 
                 <div class="mb-3">
+                    <div class="main-color"><p>{{ __('auth.password_format') }}</p></div>
                     <div class="input-group">
                         <div class="flex-grow-1 form-floating">
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password" placeholder="">
                             <label for="password">{{ __('validation.attributes.password') }} <span class="text-danger">*</span></label>
                         </div>
-                        <span class="input-group-text" onclick="password_show_hide();">
+{{--                        <span class="input-group-text" onclick="password_show_hide();">--}}
+                        <span class="input-group-text">
                             <i class="fas fa-eye" id="show_eye"></i>
                             <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
                           </span>
@@ -117,7 +119,8 @@
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password" placeholder="">
                             <label for="password-confirm" class="">{{ __('validation.attributes.password_confirm') }} <span class="text-danger">*</span></label>
                         </div>
-                            <span class="input-group-text" onclick="password_show_hide_confirm();">
+{{--                            <span class="input-group-text" onclick="password_show_hide_confirm();">--}}
+                            <span class="input-group-text">
                               <i class="fas fa-eye" id="show_eye_c"></i>
                               <i class="fas fa-eye-slash d-none" id="hide_eye_c"></i>
                             </span>
@@ -135,7 +138,7 @@
                 </div>
 
                 <div class="row mb-0 ">
-                        <button class="col-md-6 offset-md-3 cstm-btn btn btn-lg  btn-primary mb-2" type="submit"><i class="fa-solid fa-right-to-bracket main-color me-1"></i>{{ __('custom.register') }}</button>                
+                        <button class="col-md-6 offset-md-3 cstm-btn btn btn-lg  btn-primary mb-2" id="reg_btn" type="button"><i class="fa-solid fa-right-to-bracket main-color me-1"></i>{{ __('custom.register') }}</button>
                 </div>
             </form>
         </div>
@@ -144,49 +147,125 @@
 @endsection
 
 @push('scripts')
-<script>
-    $(document).ready(function () {
-        $('input[name="is_org"]').change(togglePersonal);
-        togglePersonal();
-    });
-    function togglePersonal() {
-        var show = $('input[name="is_org"]:checked').val() == 0;
-        $('#part-org').toggle(!show);
-        $('#part-person').toggle(show);
-    }
+    <script src="{{ asset('jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('jquery-validation/localization/messages_' . app()->getLocale() . '.js') }}"></script>
+    <script>
+        var validateObject = null;
 
+        function togglePersonal(init) {
+            var show = $('input[name="is_org"]:checked').val() == 0;
+            $('#part-org').toggle(!show);
+            $('#part-person').toggle(show);
+            if(typeof init != 'undefined' && validateObject){
+                validateObject.resetForm();
+                $('#reg_form').removeData('validator');
+            }
+        }
 
-    function password_show_hide_confirm() {
-    var x = document.getElementById("password-confirm");
-    var show_eye = document.getElementById("show_eye_c");
-    var hide_eye = document.getElementById("hide_eye_c");
-    hide_eye.classList.remove("d-none");
-    if (x.type === "password") {
-      x.type = "text";
-      show_eye.style.display = "none";
-      hide_eye.style.display = "block";
-    } else {
-      x.type = "password";
-      show_eye.style.display = "block";
-      hide_eye.style.display = "none";
-    }
-  }
+        function validateRegistration(){
+            //validate form
+            let lForm = $('#reg_form');
+            validateObject = $(lForm).validate({
+                // errorClass: 'is_invalid',
+                errorClass: 'text-danger mt-1',
+                errorElement: 'div',
+                rules: formRules($('#is_org2').is(':checked') ? 'person' : 'company'),
+                errorPlacement: function (error, element) {
+                    error.insertAfter(element.parent().parent());
+                },
+                invalidHandler: function (e, validation) {
+                    console.log("invalidHandler : event", e);
+                    console.log("invalidHandler : validation", validation);
+                }
+            });
+            if ($(lForm).valid()) {
+                $(lForm).submit();
+            }
+        }
 
-  function password_show_hide() {
-    var x = document.getElementById("password");
-    var show_eye = document.getElementById("show_eye");
-    var hide_eye = document.getElementById("hide_eye");
-    hide_eye.classList.remove("d-none");
-    if (x.type === "password") {
-      x.type = "text";
-      show_eye.style.display = "none";
-      hide_eye.style.display = "block";
-    } else {
-      x.type = "password";
-      show_eye.style.display = "block";
-      hide_eye.style.display = "none";
-    }
-  }
+        function formRules(formType) {
+            let rules = {
+                person: {
+                    first_name: {
+                        required: true,
+                        maxlength: 255
+                    },
+                    last_name: {
+                        required: true,
+                        maxlength: 255
+                    },
+                    email: {
+                        required: true,
+                        email: true,
+                        maxlength: 255
+                    },
+                    password: {
+                        required: true,
+                        strong_password: true,
+                    },
+                    password_confirmation: {
+                        required: true,
+                        minlength: 8,
+                        maxlength: 16,
+                    }
+                },
+                company: {
+                    org_name: {
+                        required: true,
+                        maxlength: 255
+                    },
+                    email: {
+                        required: true,
+                        email: true,
+                        maxlength: 255
+                    },
+                    password: {
+                        required: true,
+                        strong_password: true,
+                    },
+                    password_confirmation: {
+                        required: true,
+                        minlength: 8,
+                        maxlength: 16,
+                    }
+                }
+            }
+            return rules[formType];
+        }
 
-</script>
+        $(document).ready(function () {
+            $.validator.addMethod("strong_password", function (value, element) {
+                let password = value;
+                if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%&])(.{8,20}$)/.test(password))) {
+                    return false;
+                }
+                return true;
+            }, function (value, element) {
+                let password = $(element).val();
+                if (!(/^(.{8,16}$)/.test(password))) {
+                    return jQuery.validator.messages.betweenLength;
+                }
+                else if (!(/^(?=.*[A-Z])/.test(password))) {
+                    return jQuery.validator.messages.oneUpperLetter;
+                }
+                else if (!(/^(?=.*[a-z])/.test(password))) {
+                    return jQuery.validator.messages.oneLowerLetter;
+                }
+                else if (!(/^(?=.*[0-9])/.test(password))) {
+                    return jQuery.validator.messages.oneDigit;
+                }
+                else if (!(/^(?=.*[@#$%&!_=*-])/.test(password))) {
+                    return jQuery.validator.messages.oneSpecialCharacter;
+                }
+                return false;
+            });
+
+            $('#reg_btn').on('click', function (){
+                validateRegistration();
+            });
+
+            $('input[name="is_org"]').change(togglePersonal);
+            togglePersonal(1);
+        });
+    </script>
 @endpush
