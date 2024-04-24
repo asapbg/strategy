aadc912d98ecbcfdb3cfc9dd4fded52efb94600d
ASAP <noreply@asap.bg>
2024-04-24 17:50:09 +0300

    STRATEGY-284

---
+++ app/Console/Commands/seedOldLastPrisTags.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\CustomActivity;
+use App\Models\File;
+use App\Models\InstitutionLevel;
+use App\Models\Pris;
+use App\Models\StrategicDocuments\Institution;
+use App\Models\Tag;
+use App\Services\FileOcr;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class seedOldLastPrisTags extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'old:pris_tags';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate last PRIS data to application';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        activity()->disableLogging();
+        $this->info('Start at '.date('Y-m-d H:i:s'));
+        $locales = config('available_languages');
+        $ourTags = Tag::with(['translation'])->get()->pluck('id', 'translation.label')->toArray();
+        $ourPris = Pris::whereNotNull('old_id')->get()->pluck('id', 'old_id')->toArray();
+
+        //records per query
+        $step = 50;
+        //max id in old db
+        $maxOldId = DB::connection('pris')->select('select max(archimed.e_items.id) from archimed.e_items');
+        //start from this id in old database
+        //$currentStep = DB::table('pris')->select(DB::raw('max(old_id) as max'))->first()->max + 1;
+        $currentStep = 0;
+
+        if( (int)$maxOldId[0]->max ) {
+            $maxOldId = (int)$maxOldId[0]->max;
+            try {
+                while ($currentStep < $maxOldId) {
+                    echo "FromId: ".$currentStep.PHP_EOL;
+                    $oldDbResult = DB::connection('pris')->select('select
+                                pris.id as old_id,
+                                pris."xml" as to_parse_xml_details
+                            FROM archimed.e_items pris
+                            where true
+                                and pris.id >= ' . $currentStep . '
+                                and pris.id < ' . ($currentStep + $step) . '
+                                and pris.itemtypeid <> 5017 -- skip law records
+                            group by pris.id
+                            order by pris.id asc');
+
+                    if (sizeof($oldDbResult)) {
+                        foreach ($oldDbResult as $item) {
+                            $tags = [];
+
+                            $xml = simplexml_load_string($item->to_parse_xml_details);
+                            $json = json_encode($xml, JSON_UNESCAPED_UNICODE);
+                            $data = json_decode($json, true);
+
+                            //Update existing
+                            if(isset($ourPris) && sizeof($ourPris) && isset($ourPris[(int)$item->old_id])){
+                                $existPris = Pris::find($ourPris[(int)$item->old_id]);
+
+                                if($existPris){
+                                    if(isset($data['DocumentContent']) && isset($data['DocumentContent']['Attribute']) && sizeof($data['DocumentContent']['Attribute'])) {
+                                        $attributes = $data['DocumentContent']['Attribute'];
+                                        foreach ($attributes as $att) {
+                                            //get tags
+                                            if(isset($att['@attributes']) && isset($att['@attributes']['Name']) && $att['@attributes']['Name'] == 'Термини') {
+                                                if(isset($att['Value']) && isset($att['Value']['Value']) && !empty($att['Value']['Value'])) {
+                                                    //echo "Tags: ".$att['Value']['Value'].PHP_EOL;
+                                                    $tags = preg_split('/\r\n|\r|\n/', $att['Value']['Value']);
+                                                } elseif (isset($att['Value']) && !empty($att['Value']) && !isset($att['Value']['Value'])) {
+                                                    //echo "Tags: ".$att['Value'].PHP_EOL;
+                                                    $tags = preg_split('/\r\n|\r|\n/', $att['Value']);
+                                                }
+                                            }
+                                        }
+
+                                        //3. Create connection pris - tags
+                                        if(sizeof($tags)) {
+                                            $newTags = array();
+                                            foreach ($tags as $tag) {
+                                                if(!isset($ourTags[$tag])) {
+                                                    //create tag
+                                                    $newTag = \App\Models\Tag::create();
+                                                    if( $newTag ) {
+                                                        foreach ($locales as $locale) {
+                                                            $newTag->translateOrNew($locale['code'])->label = $tag;
+                                                        }
+                                                    }
+                                                    $newTag->save();
+                                                    echo "Tag with name ".$tag." created successfully".PHP_EOL;
+                                                    $ourTags[$tag] = $newTag->id;
+                                                }
+                                                $newTags[] = '('.(int)$ourTags[$tag].', '.$existPris->id.')';
+                                            }
+
+                                            DB::statement('delete from pris_tag where pris_id ='.$existPris->id);
+                                            if(sizeof($newTags)) {
+                                                DB::statement('insert into pris_tag values '.implode(',', $newTags));
+                                                $this->comment('Tags for Pris with old id '.$item->old_id.' are updated');
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    $currentStep += $step;
+                }
+            } catch (\Exception $e) {
+                Log::error('Migration old pris: ' . $e);
+            }
+        }
+        $this->info('End at '.date('Y-m-d H:i:s'));
+    }
+}
