bfac9abfb51f4d11cc96dafcbba351521e9ac014
ASAP <noreply@asap.bg>
2024-05-14 18:54:04 +0300

    add protocol connection to all documents with not empty protocol property

--- app/Console/Commands/PrisProtocolConnection.php
+++ app/Console/Commands/PrisProtocolConnection.php
@@ -31,54 +31,54 @@ class PrisProtocolConnection extends Command
     public function handle()
     {
 
-//        $maxId = Pris::where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL_DECISION)->where('doc_num', 'like', '%.%')->max('id');
-//
-//        if($maxId){
-//            $step = 50;
-//            $currentStep = 0;
-//            $stop = false;
-//
-//            while ($currentStep <= $maxId && !$stop) {
-//                $this->info('Start between '.$currentStep.' and '. ($currentStep + $step));
-//                $items = Pris::where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL_DECISION)
-//                    ->where('doc_num', 'like', '%.%')
-//                    ->where('id', '>=', $currentStep)
-//                    ->where('id', '<', $currentStep + $step)
-//                    ->get();
-//                if($items->count()){
-//                    foreach ($items as $item){
-//                        $protocol = explode('.', $item->doc_num);
-//                        if(!sizeof($protocol) == 2){
-//                            $this->clearProtocol($item);
-//                        } else{
-//                            $protocol = $protocol[0];
-//                            $dbProtocol = Pris::where('doc_num', $protocol)
-//                                ->where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL)
-//                                ->where('doc_date', '>=', Carbon::parse($item->doc_date)->startOfYear()->format('Y-m-d'))
-//                                ->where('doc_date', '<=', Carbon::parse($item->doc_date)->endOfYear()->format('Y-m-d'))
-//                                ->where('asap_last_version', 1)
-//                                ->get()
-//                                ->first();
-//
-//                            if(!$dbProtocol){
-//                                $this->clearProtocol($item);
-//                            } else{
-//                                \DB::statement('update pris set decision_protocol = '.$dbProtocol->id.' where id = '.$item->id);
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                if($currentStep == $maxId){
-//                    $stop = true;
-//                } else{
-//                    $currentStep += $step;
-//                    if($currentStep > $maxId){
-//                        $currentStep = $maxId;
-//                    }
-//                }
-//            }
-//        }
+        $maxId = Pris::where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL_DECISION)->where('doc_num', 'like', '%.%')->max('id');
+
+        if($maxId){
+            $step = 50;
+            $currentStep = 0;
+            $stop = false;
+
+            while ($currentStep <= $maxId && !$stop) {
+                $this->info('Start between '.$currentStep.' and '. ($currentStep + $step));
+                $items = Pris::where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL_DECISION)
+                    ->where('doc_num', 'like', '%.%')
+                    ->where('id', '>=', $currentStep)
+                    ->where('id', '<', $currentStep + $step)
+                    ->get();
+                if($items->count()){
+                    foreach ($items as $item){
+                        $protocol = explode('.', $item->doc_num);
+                        if(!sizeof($protocol) == 2){
+                            $this->clearProtocol($item);
+                        } else{
+                            $protocol = $protocol[0];
+                            $dbProtocol = Pris::where('doc_num', $protocol)
+                                ->where('pris.legal_act_type_id', '=', LegalActType::TYPE_PROTOCOL)
+                                ->where('doc_date', '>=', Carbon::parse($item->doc_date)->startOfYear()->format('Y-m-d'))
+                                ->where('doc_date', '<=', Carbon::parse($item->doc_date)->endOfYear()->format('Y-m-d'))
+                                ->where('asap_last_version', 1)
+                                ->get()
+                                ->first();
+
+                            if(!$dbProtocol){
+                                $this->clearProtocol($item);
+                            } else{
+                                \DB::statement('update pris set decision_protocol = '.$dbProtocol->id.' where id = '.$item->id);
+                            }
+                        }
+                    }
+                }
+
+                if($currentStep == $maxId){
+                    $stop = true;
+                } else{
+                    $currentStep += $step;
+                    if($currentStep > $maxId){
+                        $currentStep = $maxId;
+                    }
+                }
+            }
+        }
 
         //Other than protocol decisions
         $maxId = Pris::where('pris.legal_act_type_id', '<>', LegalActType::TYPE_PROTOCOL_DECISION)->where('doc_num', 'like', '%.%')->max('id');
@@ -105,7 +105,7 @@ class PrisProtocolConnection extends Command
                         } else{
                             preg_match('/^([\d]{1,})(\/)([\d\.\d\.\d]{8})\s*(.*?)(Т.|т.)([\d]{1,})/', $protocol, $matches);
                             if(!sizeof($matches) == 7){
-                                preg_match('/^([\d]{1,})(\.)([\d]{1,})/', $protocol, $matches);
+                                preg_match('/^([\d]{1,})(\.)([\d]{1,})$/', $protocol, $matches);
                                 if(!sizeof($matches) == 4){
                                     $this->clearProtocol($item);
                                 } else{
