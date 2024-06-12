956a4e8c9070c627263813918d88830af2f369ae
ASAP <noreply@asap.bg>
2024-06-12 11:39:16 +0300

    disable pik eauth

--- resources/views/eauth/create_user.blade.php
+++ resources/views/eauth/create_user.blade.php
@@ -9,13 +9,13 @@
             <div class="card-body">
                 <form method="POST" action="{{ route('eauth.user.create') }}">
                     @csrf
-                    <input type="hidden" name="person_identity" value="{{ $userInfo['person_identity'] ?? '' }}">
+{{--                    <input type="hidden" name="person_identity" value="{{ $userInfo['person_identity'] ?? '' }}">--}}
                     <input type="hidden" name="company_identity" value="{{ $userInfo['company_identity'] ?? '' }}">
                     <input type="hidden" name="org_name" value="{{ $userInfo['org_name'] ?? '' }}">
-                    <input type="hidden" name="first_name" value="{{ $userInfo['first_name'] ?? '' }}">
-                    <input type="hidden" name="middle_name" value="{{ $userInfo['middle_name'] ?? '' }}">
-                    <input type="hidden" name="last_name" value="{{ $userInfo['last_name'] ?? '' }}">
-                    <input type="hidden" name="phone" value="{{ $userInfo['phone'] ?? '' }}">
+{{--                    <input type="hidden" name="first_name" value="{{ $userInfo['first_name'] ?? '' }}">--}}
+{{--                    <input type="hidden" name="middle_name" value="{{ $userInfo['middle_name'] ?? '' }}">--}}
+{{--                    <input type="hidden" name="last_name" value="{{ $userInfo['last_name'] ?? '' }}">--}}
+{{--                    <input type="hidden" name="phone" value="{{ $userInfo['phone'] ?? '' }}">--}}
                     <div class="row">
                         @if(isset($validationErrors) && sizeof($validationErrors))
                             <p class="text-danger fs-6">
@@ -25,12 +25,23 @@
                             </p>
                         @endif
                     </div>
-                    <div class="row">
-                        <div class="form-group form-group-sm col-12 mb-3">
-                            <label class="form-label fw-semibold" for="email">{{ __('eauth.label_email') }}: <span class="required">*</span></label>
-                            <input class="form-control form-control-sm @if(isset($validationErrors) && isset($validationErrors['email'])) is-invalid @endif" type="email" value="{{ $userInfo['email'] ?? '' }}" name="email" id="email" autocomplete="off">
+                    @foreach(['first_name', 'middle_name', 'last_name', 'phone', 'person_identity', 'email'] as $v)
+                        @php
+                            $defaultValue = old($v, $userInfo[$v] ?? '');
+                            if($v == 'email') {
+                              $type = 'email';
+                            } else {
+                              $type = 'text';
+                            }
+                        @endphp
+                        <div class="row mb-3">
+                            <input type="hidden" name="{{ $v }}" value="{{ $defaultValue }}">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('custom.'.$v) }}</label>
+                            <div class="col-md-6">
+                                <input id="{{ $v }}" type="{{ $type }}" class="form-control @error(isset($validationErrors) && isset($validationErrors[$v])) is-invalid @enderror" @if($v == 'email') name="{{ $v }}" @endif value="{{ $defaultValue }}" @if($v != 'email') readonly disabled @endif>
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
                     <div class="row">
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary mt-3">{{ __('eauth.register_with_this_mail') }}</button>
