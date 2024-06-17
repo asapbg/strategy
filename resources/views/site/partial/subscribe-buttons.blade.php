29f3c5daea7a472cece379257b22687b054045a7
ASAP <noreply@asap.bg>
2024-06-17 11:52:41 +0300

    STRATEGY-362

--- resources/views/site/partial/subscribe-buttons.blade.php
+++ resources/views/site/partial/subscribe-buttons.blade.php
@@ -1,4 +1,4 @@
-@auth()
+
 {{--    @if(App\Models\User::isSubscribed(App\Models\UserSubscribe::CHANNEL_RSS))--}}
     @if(isset($hasSubscribeRss) && $hasSubscribeRss)
         <button class="btn rss-sub main-color unsubscribe" @if(isset($subscribe_params)) data-filter="{{ json_encode($subscribe_params) }}" @endif data-channel="{{ App\Models\UserSubscribe::CHANNEL_RSS }}">
@@ -16,6 +16,7 @@
 {{--            <i class="fas fa-square-rss text-warning"></i>{{ __('custom.rss_subscribe') }}--}}
 {{--        </button>--}}
     @endif
+@auth()
 {{--    @if(App\Models\User::isSubscribed(App\Models\UserSubscribe::CHANNEL_EMAIL))--}}
     @if(!isset($no_email_subscribe) || !$no_email_subscribe)
         @if(isset($hasSubscribeEmail) && $hasSubscribeEmail)
