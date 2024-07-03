283716402b5b056fcd57e3f5a48e0910b91216a6
ASAP <noreply@asap.bg>
2024-07-03 15:40:24 +0300

    notes 02.07.2024

--- app/Notifications/LegislativeInitiativeClosed.php
+++ app/Notifications/LegislativeInitiativeClosed.php
@@ -47,7 +47,7 @@ class LegislativeInitiativeClosed extends Notification
         $this->data = array('item' => $this->item);
 
         if($this->action != 'deleted'){
-            $this->data[] = ['url' => route('legislative_initiatives.view', $notifiable->id)];
+            $this->data['url'] = route('legislative_initiatives.view', $notifiable->id);
         }
 
         return (new MailMessage)
