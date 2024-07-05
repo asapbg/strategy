81b680cdd7e82e3b40996952d1ed523a893401ab
ASAP <noreply@asap.bg>
2024-07-05 09:34:01 +0300

    email template logo fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,16 +1,16 @@
 <tr>
-<td class="header">
-<div class="contact-info">
-<div class="float-left">
-<a class="navbar-brand logo-link" href="{{ $url }}">
-<img src="{{ asset('/img/strategy-logo.svg') }}" alt="{{ __('site.seo_title') }}" id="siteLogo">
-</a>
-</div>
-<div class="float-left text-left">
-<b class="main-color fw-600" id="ms">{{ __('site.seo_description') }}</b><br>
-<b class="fw-600" id="ok">{{ __('site.seo_title') }}</b>
-</div>
-</div>
-</td>
+    <td class="header">
+        <div class="contact-info">
+            <div>
+                <a class="navbar-brand logo-link" href="{{ $url }}" style="width: 80px; display: block;">
+                    <img src="{{ asset('/img/strategy-logo.svg') }}" alt="{{ __('site.seo_title') }}" id="siteLogo">
+                </a>
+            </div>
+            <div class="text-left">
+                <b class="main-color fw-600" id="ms">{{ __('site.seo_description') }}</b><br>
+                <b class="fw-600" id="ok">{{ __('site.seo_title') }}</b>
+            </div>
+        </div>
+    </td>
 </tr>
 
