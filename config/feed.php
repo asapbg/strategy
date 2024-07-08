c29ba15ebd47c512319e721ae61af128e39379c2
ASAP <noreply@asap.bg>
2024-07-08 11:18:47 +0300

    notes 05.07.24 api

--- config/feed.php
+++ config/feed.php
@@ -555,56 +555,7 @@ return [
             /*
              * The feed will be available on this url.
              */
-            'url' => '/public-consultation-test.rss',
-
-            'title' => '[Strategy.bg] Нови консултации',
-            'description' => '',
-            'language' => 'bg-BG',
-
-            /*
-             * The image to display for the feed. For Atom feeds, this is displayed as
-             * a banner/logo; for RSS and JSON feeds, it's displayed as an icon.
-             * An empty value omits the image attribute from the feed.
-             */
-            'image' => '',
-
-            /*
-             * The format of the feed. Acceptable values are 'rss', 'atom', or 'json'.
-             */
-            'format' => 'atom',
-
-            /*
-             * The view that will render the feed.
-             */
-            'view' => 'feed::pc_feed',
-
-            /*
-             * The mime type to be used in the <link> tag. Set to an empty string to automatically
-             * determine the correct value.
-             */
-            'type' => '',
-
-            /*
-             * The content type for the feed response. Set to an empty string to automatically
-             * determine the correct value.
-             */
-            'contentType' => '',
-        ],
-        'public_consultation3' => [
-            /*
-             * Here you can specify which class and method will return
-             * the items that should appear in the feed. For example:
-             * [App\Model::class, 'getAllFeedItems']
-             *
-             * You can also pass an argument to that method. Note that their key must be the name of the parameter:
-             * [App\Model::class, 'getAllFeedItems', 'parameterName' => 'argument']
-             */
-            'items' => 'App\Models\Consultations\PublicConsultation@getFeedItems',
-
-            /*
-             * The feed will be available on this url.
-             */
-            'url' => '/public-consultation-test2.rss',
+            'url' => '/public-consultation-test3.rss',
 
             'title' => '[Strategy.bg] Нови консултации',
             'description' => '',
--- resources/views/vendor/feed/pc_feed.blade.php
+++ resources/views/vendor/feed/pc_feed.blade.php
@@ -3,38 +3,43 @@
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="{{ $meta['language'] }}">
+    <icon>{{ asset('img/strategy-logo.svg') }}</icon>
     @foreach($meta as $key => $metaItem)
-        @if($key === 'link')
-            <{{ $key }} href="{{ url($metaItem) }}" rel="self"></{{ $key }}>
-        @elseif($key === 'title')
+        @if($key === 'title')
             <{{ $key }}>{!! \Spatie\Feed\Helpers\Cdata::out($metaItem) !!}</{{ $key }}>
-        @elseif($key === 'description')
-            <subtitle>{{ $metaItem }}</subtitle>
-        @elseif($key === 'language')
-        @elseif($key === 'image')
-            @if(!empty($metaItem))
-                <logo>{!! $metaItem !!}</logo>
-            @else
-
-            @endif
-        @else
+        @elseif($key === 'link')
+            <{{ $key }} href="{{ url($metaItem) }}" rel="self"></{{ $key }}>
+        @elseif(!in_array($key, ['description', 'language', 'image', '']))
             <{{ $key }}>{{ $metaItem }}</{{ $key }}>
         @endif
-  @endforeach
+{{--        @if($key === 'link')--}}
+{{--            <{{ $key }} href="{{ url($metaItem) }}" rel="self"></{{ $key }}>--}}
+{{--        @elseif($key === 'title')--}}
+{{--            <{{ $key }}>{!! \Spatie\Feed\Helpers\Cdata::out($metaItem) !!}</{{ $key }}>--}}
+{{--        @elseif($key === 'description')--}}
+{{--            <subtitle>{{ $metaItem }}</subtitle>--}}
+{{--        @elseif($key === 'language')--}}
+{{--        @elseif($key === '')--}}
+{{--            @if(!empty($metaItem))--}}
+{{--                <logo>{!! $metaItem !!}</logo>--}}
+{{--            @else--}}
+
+{{--            @endif--}}
+{{--        @else--}}
+{{--            <{{ $key }}>{{ $metaItem }}</{{ $key }}>--}}
+{{--        @endif--}}
+    @endforeach
     @foreach($items as $item)
         <entry>
-            <title>{{ $item->title }}</title>
-            <link rel="alternate" href="{{ url($item->link) }}"/>
-            <id>{{ url($item->link) }}</id>
-            @foreach($item->category as $category)
-                <category term="{{ $category }}" />
-            @endforeach
             <author>
                 <name>{{ $item->authorName }}</name>
                 @if(isset($item->authorUrl) && $item->authorUrl)
-                    <url>{{ $item->authorUrl }}</url>
+                    <uri>{{ $item->authorUrl }}</uri>
                 @endif
             </author>
+            @foreach($item->category as $category)
+                <category label="{{ $category }}" term="{{ $category }}" />
+            @endforeach
             @if(isset($item->enclosure) && $item->enclosure)
                 <summary type="html">
                     <description><![CDATA[
@@ -43,9 +48,16 @@
                 </summary>
             @endif
             <content type="html"><![CDATA[
+                @if(isset($item->enclosure) && $item->enclosure)
+                    <img alt="{{ $item->title }}" src="{{ url($item->enclosure) }}" />
+                @endif
                 {!! $item->summary !!}
                 ]]></content>
+            <id>{{ url($item->id) }}</id>
+            <link href="{{ url($item->link) }}"/>
             <updated>{{ \Carbon\Carbon::parse($item->updated)->toAtomString() }}</updated>
+            <published>@if(isset($item->published)){{ \Carbon\Carbon::parse($item->published)->toAtomString() }}@else{{ \Carbon\Carbon::parse($item->created)->toAtomString() }}@endif</published>
+            <title>{{ $item->title }}</title>
         </entry>
     @endforeach
 </feed>
