d74879c7eb5c205bb186712707bc1e575fb86574
ASAP <noreply@asap.bg>
2024-07-10 16:05:50 +0300

    optimization images

--- app/Http/Controllers/Admin/Ogp/OgpNewsController.php
+++ app/Http/Controllers/Admin/Ogp/OgpNewsController.php
@@ -142,6 +142,7 @@ class OgpNewsController extends AdminController
                         $file->content_type = $itemImg->getClientMimeType();
                         $file->sys_user = $request->user()->id;
                         $file->save();
+                        generateImageThumbnail($file);
                     }
                 } else{
                     $file = new File([
@@ -157,6 +158,7 @@ class OgpNewsController extends AdminController
                     if( $file ) {
                         $item->file_id = $file->id;
                         $item->save();
+                        generateImageThumbnail($file);
                     }
                 }
             }
