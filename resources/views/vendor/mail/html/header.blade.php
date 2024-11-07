2b31bb12a36793c26b64dd9503d58c091e22522d
ASAP <noreply@asap.bg>
2024-11-07 08:53:16 +0200

    remove logo from emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,12 @@
 <tr>
     <td class="header">
-        <div class="contact-info" style="display: flex;">
-            <div>
-                <a class="navbar-brand logo-link" href="{{ $url }}" style="width: 80px; display: block;">
-                    <img src="{{ asset('/img/strategy-logo.svg') }}" alt="{{ __('site.seo_title') }}" id="siteLogo">
-                </a>
-            </div>
+        <div class="contact-info">
+{{--        <div class="contact-info" style="display: flex;">--}}
+{{--            <div>--}}
+{{--                <a class="navbar-brand logo-link" href="{{ $url }}" style="width: 80px; display: block;">--}}
+{{--                    <img src="{{ asset('/img/strategy-logo.svg') }}" alt="{{ __('site.seo_title') }}" id="siteLogo">--}}
+{{--                </a>--}}
+{{--            </div>--}}
             <div class="text-left">
                 <b class="main-color fw-600" id="ms">{{ __('site.seo_description') }}</b><br>
                 <b class="fw-600" id="ok">{{ __('site.seo_title') }}</b>
