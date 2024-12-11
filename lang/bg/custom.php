94ca816e5d724ce53a65e6c4b32207428d3f40f7
Vasil Vasilev <vasil.vasilev@asap.bg>
2024-12-11 15:18:55 +0200

    STRATEGY-429 - Another label change

--- lang/bg/custom.php
+++ lang/bg/custom.php
@@ -654,7 +654,7 @@ return [
     'type_of_governing' => 'Вид орган',
     'public_consultation_link_missing'   => 'Няма връзка с обществена консултация',
     'strategic_document_link_missing'   => 'Няма връзка с друг стратегически документ',
-    'date_indefinite_name'      => 'Безсрочна',
+    'date_indefinite_name'      => 'Не е указан срок',
     'presence_npo_representative' => 'Наличие на представител на НПО в състава на съвета',
     'names_jobs_organization' => 'имена, длъжност, организация',
     'parent_strategic_document' =>  'Документът има връзка със следния документ',
