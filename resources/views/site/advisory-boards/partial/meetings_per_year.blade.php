40cc70725f6b05c4e2665d330c02f271563042f8
ASAP <noreply@asap.bg>
2024-12-20 16:16:31 +0200

    STRATEGY-522

--- resources/views/site/advisory-boards/partial/meetings_per_year.blade.php
+++ resources/views/site/advisory-boards/partial/meetings_per_year.blade.php
@@ -3,7 +3,7 @@
 @endphp
 
 @if($item->meetings_per_year)
-    <h5 class="{{ $class }}">{{ __('validation.attributes.meetings_per_year') . ' ' . $item->meetings_per_year }}</h5>
+    <h5 class="{{ $class }}">{{ __('validation.attributes.meetings_per_year') . ': ' . $item->meetings_per_year }}</h5>
 @else
     <h5 class="{{ $class }}">{{ __('custom.no_meetings_per_year') }}</h5>
 @endif
