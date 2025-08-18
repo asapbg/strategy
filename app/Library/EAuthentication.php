afcff417becd3b68e66b4dc59a0a7dba9ac6ab4c
Vasil Vasilev <vasil.vasilev@asap.bg>
2025-08-18 16:50:43 +0300

    STRATEGY-581

--- app/Library/EAuthentication.php
+++ app/Library/EAuthentication.php
@@ -244,7 +244,7 @@ class EAuthentication
             //  Examine the fully decrypted XML document:
 
             $response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $xml->getXml());
-            $xml = simplexml_load_string($response);
+            $xml = simplexml_load_string(mb_convert_encoding($response, 'UTF-8'));
 
         } else {
             $response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $message);
