6e9dab7d54356614abf7ccf8eb38b3de13379ede
Vasil Vasilev <vasil.vasilev@asap.bg>
2025-11-07 17:39:14 +0200

    STRATEGY-666 - Rename server key to secret key

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -2,5 +2,5 @@
 
 return [
     'site_key' => env('RECAPTCHA_SITE_KEY', '6Le8hAUsAAAAAA-I4a97Ptd8D_5UXiMisRwlHwOh'),
-    'secret_key' => env('RECAPTCHA_SERVER_KEY', '6Le8hAUsAAAAAHPr8s1042VWU8ItgbmwfYQZY9se'),
+    'secret_key' => env('RECAPTCHA_SECRET_KEY', '6Le8hAUsAAAAAHPr8s1042VWU8ItgbmwfYQZY9se'),
 ];
