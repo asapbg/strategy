86b301b3b731b072f5863b8cea93bcf1b00834ce
ASAP <noreply@asap.bg>
2025-11-11 14:25:58 +0200

    STRATEGY-667

---
+++ database/migrations/2025_11_07_175328_fix_pc_files.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\File;
+use App\Models\Timeline;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        File::where('id', 1312527)->delete();
+        $pc_id = 11660;
+        //File::where('id_object', $pc_id)->where('code_object', File::CODE_OBJ_PUBLIC_CONSULTATION)->where('created_at', '>', '2025-11-06 17:53:20')->update(['created_at' => '2025-10-10 10:01:13','updated_at' => '2025-10-09 10:01:13']);
+        //Timeline::where('object_type', File::class)->where('public_consultation_id', $pc_id)->where('created_at', '>', '2025-11-06 17:53:20')->update(['created_at' => '2025-10-10 10:01:13','updated_at' => '2025-10-09 10:01:13']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
