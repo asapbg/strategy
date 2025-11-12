28414079758227e5a0b63f0b58fcb2012150a100
ASAP <noreply@asap.bg>
2025-11-12 14:17:05 +0200

    STRATEGY-674

--- database/migrations/2025_11_12_134536_add_comments_to_pc_11451.php
+++ database/migrations/2025_11_12_134536_add_comments_to_pc_11451.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\File;
+use App\Models\Timeline;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -14,6 +16,10 @@ return new class extends Migration
      */
     public function up()
     {
+        $file_id = 1312658;
+        File::where('id', $file_id)->delete();
+        Timeline::where('object_type', File::class)->where('object_id', $file_id)->delete();
+
         $sqlFilePath = database_path('data/comments-12.11.2025.sql');
 
         $sql = file_get_contents($sqlFilePath);
