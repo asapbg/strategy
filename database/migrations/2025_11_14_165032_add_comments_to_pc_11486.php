ffef5dbcf30942211738bb381ec6b24243240735
ASAP <noreply@asap.bg>
2025-11-14 16:52:59 +0200

    STRATEGY-678

---
+++ database/migrations/2025_11_14_165032_add_comments_to_pc_11486.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $sqlFilePath = database_path('data/comments_pc_11486');
+
+        $sql = file_get_contents($sqlFilePath);
+
+        DB::connection()->getPdo()->exec($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pc_11486', function (Blueprint $table) {
+            //
+        });
+    }
+};
