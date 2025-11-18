6f97c59d8e7cc1020a5db4668bf7b58304fad9ba
ASAP <noreply@asap.bg>
2025-11-18 15:30:04 +0200

    test with order Р-200 and Р-202

--- app/Console/Commands/PopulateFileTextInDatabase.php
+++ app/Console/Commands/PopulateFileTextInDatabase.php
@@ -39,6 +39,7 @@ class PopulateFileTextInDatabase extends Command
             ->where('files.locale', '=', 'bg')
             ->whereRaw("(file_text is null or file_text = '')")
             ->where('code_object', File::CODE_OBJ_PRIS)
+            ->whereIn('id_object', [167933, 167939])
             ->whereNotIn('content_type', [
                 'application/vnd.ms-excel',
                 'application/x-rar',
