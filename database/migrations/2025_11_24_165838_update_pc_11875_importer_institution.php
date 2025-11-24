2e57353bb39939c9f5699a541a438efc39f1e212
ASAP <noreply@asap.bg>
2025-11-24 17:01:45 +0200

    STRATEGY-692

---
+++ database/migrations/2025_11_24_165838_update_pc_11875_importer_institution.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $importer = \App\Models\StrategicDocuments\InstitutionTranslation::where('name', 'Общинска администрация - Търговище')->first();
+        if ($importer) {
+            \App\Models\Consultations\PublicConsultation::where('id', 11875)->update(['importer_institution_id' => $importer->id]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
