5b645b1676840c8a9721426fbb59e21f004f0013
ASAP <noreply@asap.bg>
2025-11-25 15:21:40 +0200

    STRATEGY-699

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -35,13 +35,13 @@
         var municipalityLevel = "{{ \App\Enums\InstitutionCategoryLevelEnum::MUNICIPAL->value }}";
     </script>
 
-    @if(env('APP_ENV') != 'local')
+    @if(env('APP_ENV') == 'production')
         <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
+            (function(c,l,a,r,i,t,y) {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mxhrfku29f");
+            })(window, document, "clarity", "script", "ub34b42l7i");
         </script>
     @endif
 </head>
