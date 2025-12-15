a1b5ca0c2f782a71ea12e7f62cc734774cc4d081
ASAP <noreply@asap.bg>
2025-12-15 16:01:52 +0200

    STRATEGY-718 user guide

--- resources/views/site/partial/guide.blade.php
+++ resources/views/site/partial/guide.blade.php
@@ -2,19 +2,25 @@
 
 @section('content')
     <section class="content container">
-{{--        <div class="page-title mb-md-3 mb-2 px-5">--}}
-{{--            <h3 class="b-1 text-center">{{ $pageTitle }}</h3>--}}
-{{--        </div>--}}
-        <div class="card card-light mb-4">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="row mt-3">
-                            <embed src="{{ asset(DIRECTORY_SEPARATOR.'help'.DIRECTORY_SEPARATOR.$file) }}" width="800px" height="2100px" />
+        @php
+            $pdfUrl = asset(DIRECTORY_SEPARATOR.'help'.DIRECTORY_SEPARATOR.$file);
+        @endphp
+        @if(is_android())
+            <a class="main-color text-decoration-none d-block my-5" href="{{ $pdfUrl }}">
+                {!! fileIcon('application/pdf') !!} {{ __('site.user_guide') }}
+            </a>
+        @else
+            <div class="card card-light mb-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row mt-3">
+                                <embed src="{{ $pdfUrl }}" width="800px" height="2100px" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     </section>
 @endsection
